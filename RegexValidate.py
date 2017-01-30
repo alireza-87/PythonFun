503268bb4123ed6f3e478af4a3c32016ad2cd6e5
AlirezaKarimi <alireza.karimi.67@gmail.com>
2017-01-30 14:33:56 +0330

    fix warning

--- RegexValidate.py
+++ RegexValidate.py
@@ -1,10 +1,15 @@
+# Just A Fun Python2.7 Script
+# AlirezaKarimi
+# Email:Alireza.karimi.67@gmail.com
+# Tell:+989362397176
+
 import re
-inputString=['https://www.gapafzar.com/groupname','http://www.gapafzar.com/groupname','www.gapafzar.com/groupname','https://www.gapafzar.com/groupname/Test','http://www.gapafzar.com/groupname/Test','www.gapafzar.com/groupname/Test','www.google.com','asdhttps://www.gapafzar.com/groupname/Test']
+inputString = ['https://www.gapafzar.com/groupname','http://www.gapafzar.com/groupname','www.gapafzar.com/groupname','https://www.gapafzar.com/groupname/Test','http://www.gapafzar.com/groupname/Test','www.gapafzar.com/groupname/Test','www.google.com','asdhttps://www.gapafzar.com/groupname/Test']
 pattern = '(http[s]?:\/\/(www\.)?|w{3}\.)(gapafzar\.com\/join(\/.*)|gapafzar\.com\/(\w{1,}))'
-prog=re.compile(pattern)
+prog = re.compile(pattern)
 for value in inputString:
     if prog.match(value):
-        print " result : ",' MATCH ',' value : ',value
+        print " result : ", ' MATCH ', ' value : ', value
     else:
-        print " result : ",' FAIL  ',' value : ',value
+        print " result : ", ' FAIL  ', ' value : ', value
 raw_input()
